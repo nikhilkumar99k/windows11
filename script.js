ee236c7bc0cc5f1c0323e16bdbf64fb58063c40e
Nikhil Kumar <91065041+nikhilkumar99k@users.noreply.github.com>
2022-06-27 05:57:20 +0530

    add

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let taskbar = document.getElementsByClassName("icon")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
+
+let rightpic = document.getElementsByClassName("right")[0]
+let datemenu = document.getElementsByClassName("date")[0]
+
+rightpic.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(datemenu.style.bottom == "50px"){
+        datemenu.style.bottom = "-655px"
+    }
+    else{
+        datemenu.style.bottom = "50px"
+    }
+})
